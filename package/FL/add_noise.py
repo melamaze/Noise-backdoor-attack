7d541f1fbb9c10919b9aec584d4576ef2ca6feaa
hentci <1222bruce@gmail.com>
2022-12-20 15:23:51 +0800

    [add] new util

--- package/FL/add_noise.py
+++ package/FL/add_noise.py
@@ -13,10 +13,42 @@ def add_gaussian(image):
     # Split the image into separate color channels
     red_channel, green_channel, blue_channel = cv2.split(image_array)
 
-    # Generate noise matrices for each color channel with standard deviation 0.9
-    red_noise = np.random.normal(0, 0.9, red_channel.shape)
-    green_noise = np.random.normal(0, 0.9, green_channel.shape)
-    blue_noise = np.random.normal(0, 0.9, blue_channel.shape)
+    # Generate noise matrices for each color channel with standard deviation 4.0
+    red_noise = np.random.normal(0, 4.0, red_channel.shape)
+    green_noise = np.random.normal(0, 4.0, green_channel.shape)
+    blue_noise = np.random.normal(0, 4.0, blue_channel.shape)
+
+    # Add the noise to each color channel
+    noisy_red = red_channel + red_noise
+    noisy_green = green_channel + green_noise
+    noisy_blue = blue_channel + blue_noise
+
+    # Combine the noisy color channels into a single image
+    noisy_image = cv2.merge([noisy_red, noisy_green, noisy_blue])
+
+    # Convert the noisy image back to an image and save it
+    noisy_image = np.uint8(noisy_image)
+
+    # Convert the image from the OpenCV format to the PIL format
+    pil_image = Image.fromarray(cv2.cvtColor(noisy_image, cv2.COLOR_BGR2RGB))
+
+    return pil_image
+
+
+def add_uniform(image):
+
+    image = cv2.cvtColor(np.asarray(image), cv2.COLOR_RGB2BGR)
+
+    # Convert the image to a numpy array
+    image_array = np.array(image)
+
+    # Split the image into separate color channels
+    red_channel, green_channel, blue_channel = cv2.split(image_array)
+
+    # Generate noise matrices for each color channel with uniform distribution 
+    red_noise = np.random.uniform(-2.0, 2.0, red_channel.shape)
+    green_noise = np.random.uniform(-2.0, 2.0, green_channel.shape)
+    blue_noise = np.random.uniform(-2.0, 2.0, blue_channel.shape)
 
     # Add the noise to each color channel
     noisy_red = red_channel + red_noise
